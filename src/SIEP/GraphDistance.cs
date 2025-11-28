b3378898b55dd92d16b653ccbc34e9c957108007
adaszkiewicza <ada.antek@gmail.com>
2025-11-28 20:38:51 +0100

    Fix NaN sqrt issue

--- src/SIEP/GraphDistance.cs
+++ src/SIEP/GraphDistance.cs
@@ -15,7 +15,7 @@ internal static class GraphDistance
         var kernelAtoB = featureA.CalculateKernel(featureB);
         
         // kernel to pseudometric formula
-        return Math.Sqrt(1 - kernelAtoB / (Math.Sqrt(kernelAtoA * kernelBtoB)));
+        return Math.Sqrt(1 - kernelAtoB / (Math.Sqrt(kernelAtoA) * Math.Sqrt(kernelBtoB)));
     }
 
     private static (WlFeatureMap, WlFeatureMap) MakeWlFeatureMaps(Graph graphA, Graph graphB, int iterations = 4)
